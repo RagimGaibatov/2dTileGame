0fe24fcc4ff0f866223647a12cbccb03f0540003
Ragim Gaibatov <ragimgaibatov@gmail.com>
2022-03-19 19:48:26 +0300

    Animation State

--- Assets/Scripts/PlayerMovements.cs
+++ Assets/Scripts/PlayerMovements.cs
@@ -10,10 +10,12 @@ public class PlayerMovements : MonoBehaviour
     [SerializeField] private float runSpeed = 5f;
     private Vector2 moveInput;
     private Rigidbody2D myRigidbody;
+    private Animator myAnimator;
 
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
 
@@ -33,6 +35,9 @@ public class PlayerMovements : MonoBehaviour
     {
         Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, myRigidbody.velocity.y);
         myRigidbody.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("IsRunning", playerHasHorizontalSpeed);
     }
 
     void FlipSprite()
@@ -41,7 +46,7 @@ public class PlayerMovements : MonoBehaviour
 
         if (playerHasHorizontalSpeed)
         {
-            transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1f); 
+            transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1f);
         }
     }
 }
