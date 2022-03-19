2c105bf2f302ab0d1a3d35561dba3daae7ced73b
Ragim Gaibatov <ragimgaibatov@gmail.com>
2022-03-19 14:14:46 +0300

    Set up Input System

---
+++ Assets/Scripts/PlayerMovements.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovements : MonoBehaviour
+{
+    private Vector2 moveInput;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+        Debug.Log(moveInput);
+    }
+}
