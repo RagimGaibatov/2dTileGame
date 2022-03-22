14425e184ce3e096e73ff01843ebff055072fc21
Ragim Gaibatov <ragimgaibatov@gmail.com>
2022-03-22 16:07:17 +0300

    Added Enemy

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    private Rigidbody2D myRigidbody;
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        myRigidbody.velocity = new Vector2(moveSpeed, 0f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        moveSpeed = -moveSpeed;
+        FlipEnemyFacing();
+    }
+
+    void FlipEnemyFacing()
+    {
+        transform.localScale = new Vector2(-(Math.Sign(myRigidbody.velocity.x)), 1f);
+    }
+}
